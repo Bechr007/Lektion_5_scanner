eeb870da7005c57afcb869f8c28212f7d617f723
Bechr Birkdar <bechr007@gmail.com>
2023-09-19 13:14:29 +0200

    scanner practice

--- src/com/bechr/scanner/Main.java
+++ src/com/bechr/scanner/Main.java
@@ -1,8 +1,43 @@
 package com.bechr.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("app is running");
+        boolean isPlaying = true;
+
+       do {
+           int result = scannerNumber();
+
+           if (result > 1000){
+               isPlaying = false;
+           } else {
+
+               for (int i = 0; i < result; i++) {
+                   System.out.println(i);
+                   System.out.println("loading...");
+               }
+           }
+
+       } while (isPlaying);
+
     }
+
+    public static int scannerNumber(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("insert a number");
+
+        return sc.nextInt();
+    }
+
+    public static String scannerText(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("insert a name");
+        String input = sc.nextLine();
+        return input;
+    }
+
 }
