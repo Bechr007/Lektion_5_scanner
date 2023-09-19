acc7caf85d473907551e858a46654eb2e504cb9c
Bechr Birkdar <bechr007@gmail.com>
2023-09-19 13:41:44 +0200

    Student scanner implementation

--- src/com/bechr/scanner/Main.java
+++ src/com/bechr/scanner/Main.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Objects
         Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
 
+        // Game
         do {
 
-            System.out.println("input number!");
+            System.out.println("input benny's age!");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("you picked: " + result );
+            System.out.println("Benny's age is: " + benny.age);
 
         } while (true);
 
---
+++ src/com/bechr/scanner/Student.java
@@ -0,0 +1,9 @@
+package com.bechr.scanner;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public boolean isTired;
+
+}
